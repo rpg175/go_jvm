0afeaff03a6f8bf820b4cb4d0f7dedc3e1f99920
ko <kojavaee@aliyun.com>
2021-10-14 08:48:52 +0800

    添加一些go code

---
+++ coolcode/code_slice_interface_time_performance.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"unsafe"
+)
+
+type slice struct {
+	array unsafe.Pointer
+	len int
+	cap int
+}
+
+func main() {
+	path := []byte("AAAA/BBBBBBBBB")
+	sepIndex := bytes.IndexByte(path,'/')
+
+	dir1 := path[:sepIndex]
+	dir2 := path[sepIndex+1:]
+
+	fmt.Println("dir1 =>",string(dir1))
+	fmt.Println("dir1 =>",string(dir2))
+
+	dir1 = append(dir1,"suffix"...)
+
+	fmt.Println("dir1 =>",string(dir1))
+	fmt.Println("dir2 =>",string(dir2))
+}
