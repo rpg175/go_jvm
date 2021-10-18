e7172d9b40c98a82a640ecd92189096ecdcbd433
ko <kojavaee@aliyun.com>
2021-10-18 18:52:31 +0800

    add function options

--- coolcode/functional_options.go
+++ coolcode/functional_options.go
@@ -14,4 +14,19 @@ type Server struct {
 	TLS *tls.Config
 }
 
+func NewDefaultServer(addr string, port int) (*Server,error) {
+	return &Server{addr,port,"tcp",30 * time.Second,100,nil},nil
+}
+
+func NewTLSServer(addr string,port int ,tls *tls.Config) (*Server,error) {
+	return &Server{addr,port,"tcp",30*time.Second,100,tls},nil
+}
+
+func NewServerWithTimeout(addr string,port int,timeout time.Duration) (*Server, error) {
+	return &Server{addr,port,"tcp",timeout,100,nil},nil
+}
+
+func NewServerTLSServerWithMaxConnAndTimeout(addr string,port int,maxConnects int,timeout time.Duration,tls *tls.Config)(*Server, error) {
+	return &Server{addr,port,"tcp",30*time.Second,maxConnects,tls},nil
+}
 
