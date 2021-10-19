bff41bf90adebfb8f1edda740c609e022d00b350
ko <kojavaee@aliyun.com>
2021-10-19 13:39:34 +0800

    add option

--- coolcode/functional_options.go
+++ coolcode/functional_options.go
@@ -64,6 +64,48 @@ func (sb *ServerBuilder) Build() (Server) {
 	return  sb.Server
 }
 
+type Option func(*Server)
+
+func Protocol(p string) Option {
+	return func(s *Server) {
+		s.Protocol = p
+	}
+}
+
+func Timeout(timeout time.Duration) Option {
+	return func(s *Server) {
+		s.Timeout = timeout
+	}
+}
+
+func MaxConnects(maxConnect int) Option {
+	return func(s *Server) {
+		s.MaxConnects = maxConnect
+	}
+}
+
+func TLS(tls *tls.Config) Option {
+	return func(s *Server) {
+		s.TLS = tls
+	}
+}
+
+func NewServer(addr string, port int, options ...func(*Server)) (*Server, error) {
+	srv := Server{
+		Addr:     addr,
+		Port:     port,
+		Protocol: "tcp",
+		Timeout:  30 * time.Second,
+		MaxConnects: 1000,
+		TLS:      nil,
+	}
+	for _, option := range options {
+		option(&srv)
+	}
+	//...
+	return &srv, nil
+}
+
 func main() {
 	//sb := ServerBuilder{}
 	//server, err := sb.Create("127.0.0.1", 8080).
@@ -71,4 +113,8 @@ func main() {
 	//	WithMaxConnect(1024).
 	//	WithTimeOut(30*time.Second).
 	//	Build()
+
+	//s1, _ := NewServer("localhost", 1024)
+	//s2, _ := NewServer("localhost", 2048, Protocol("udp"))
+	//s3, _ := NewServer("0.0.0.0", 8080, Timeout(300*time.Second), MaxConnects(1000))
 }
