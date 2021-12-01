9ea6c52e82d17c763fc4e164acf4ae0d22b4b7ac
ko <kojavaee@aliyun.com>
2021-12-01 18:08:20 +0800

    add reflection

--- coolcode/reflection.go
+++ coolcode/reflection.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
+// Type Assert
 type Container []interface{}
 
 func (c *Container) Put(elem interface{}) {
@@ -14,9 +18,58 @@ func (c *Container) Get() interface{} {
 	return elem
 }
 
+// Reflection
+type ReflectionContainer struct {
+	s reflect.Value
+}
+
+func NewContainer(t reflect.Type,size int) * ReflectionContainer  {
+	if size <= 0 {
+		size = 64
+	}
+	return &ReflectionContainer{s:reflect.MakeSlice(reflect.SliceOf(t),0,size)}
+}
+
+func (c *ReflectionContainer) RPut(val interface{})  error {
+	if reflect.ValueOf(val).Type() != c.s.Type().Elem() {
+		return fmt.Errorf("Put: cannot put a %T into a slice of %s", val, c.s.Type().Elem())
+	}
+	c.s = reflect.Append(c.s, reflect.ValueOf(val))
+	return nil
+}
+func (c *ReflectionContainer) RGet(refval interface{}) error {
+	if reflect.ValueOf(refval).Kind() != reflect.Ptr ||
+		reflect.ValueOf(refval).Elem().Type() != c.s.Type().Elem() {
+		return fmt.Errorf("Get: needs *%s but got %T", c.s.Type().Elem(), refval)
+	}
+	reflect.ValueOf(refval).Elem().Set( c.s.Index(0) )
+	c.s = c.s.Slice(1, c.s.Len())
+	return nil
+}
+
 func main() {
 	intContainer := &Container{}
 	intContainer.Put(7)
 	intContainer.Put(42)
-	fmt.Printf("%v",intContainer.Get())
+	elem, ok := intContainer.Get().(int)
+	if !ok {
+		fmt.Println("Unable to read an int from intContainer")
+	}
+	fmt.Printf("assertExample: %d (%T)\n", elem, elem)
+
+	f1 := 3.1415926
+	f2 := 1.41421356237
+	c := NewContainer(reflect.TypeOf(f1), 16)
+	if err := c.RPut(f1); err != nil {
+		panic(err)
+	}
+	if err := c.RPut(f2); err != nil {
+		panic(err)
+	}
+	g := 0.0
+	if err := c.RGet(&g); err != nil {
+		panic(err)
+	}
+	fmt.Printf("%v (%T)\n", g, g) //3.1415926 (float64)
+	fmt.Println(c.s.Index(0)) //1.4142135623
 }
