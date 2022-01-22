a551b8ef8c1c624a42bfd3a964a7cf9694f661d9
ko <kojavaee@aliyun.com>
2022-01-23 07:14:26 +0800

    添加go server

--- coroutine/test_coroutine.go
+++ coroutine/test_coroutine.go
@@ -68,4 +68,84 @@ func main() {
 	//}
 	//time.Sleep(time.Second * 8)
 	//fmt.Println(n)
+
+	//ch := make(chan int) //Java : ch = new BlockingQueue<Integer> (size = 1)
+	//defer close(ch)
+	//
+	//ch <- 1   // Java : ch.put(1) 阻塞方式往里塞数
+	//x := <-ch // Java : ch.take() 阻塞方式往外拿数
+	//fmt.Println(x)
+	//fatal error: all goroutines are asleep - deadlock!
+
+	//ch := make(chan int) //Java : ch = new BlockingQueue<Integer> (size = 1)
+	//defer close(ch)
+	//
+	////需要在不同的携程里面分别拿数和取数
+	//go func() {
+	//	ch <- 1 // Java : ch.put(1) 阻塞方式往里塞数
+	//}()
+	//
+	//x := <-ch // Java : ch.take() 阻塞方式往外拿数
+	//fmt.Println(x)
+	////output:1
+
+	//ch1 := make(chan int)
+	//ch2 := make(chan int)
+	//defer func() {
+	//	close(ch1)
+	//	close(ch2)
+	//}()
+	//
+	//go func() {
+	//	for i := 0; ; i++ {
+	//		ch1 <- i
+	//		time.Sleep(time.Second)
+	//	}
+	//}()
+	//
+	//go func() {
+	//	for i := 0; ; i-- {
+	//		ch2 <- i
+	//		time.Sleep(time.Second * 5)
+	//	}
+	//}()
+	//
+	//for {
+	//	x := <-ch1
+	//	y := <-ch2 // Java : ch.take() 阻塞方式往外拿数，所以导致sleep 5秒后执行
+	//	fmt.Println(x)
+	//	fmt.Println(y)
+	//}
+
+	ch1 := make(chan int)
+	ch2 := make(chan int)
+	defer func() {
+		close(ch1)
+		close(ch2)
+	}()
+
+	go func() {
+		for i := 0; ; i++ {
+			ch1 <- i
+			time.Sleep(time.Second)
+		}
+	}()
+
+	go func() {
+		for i := 0; ; i-- {
+			ch2 <- i
+			time.Sleep(time.Second * 5)
+		}
+	}()
+
+	for {
+		//select case不需要阻塞等待，谁有数据处理谁（信号量的方式）
+		select {
+		case x := <-ch1:
+			fmt.Println(x)
+		case y := <-ch2:
+			fmt.Println(y)
+		}
+	}
+
 }
