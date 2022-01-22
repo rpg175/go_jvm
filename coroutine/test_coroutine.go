f9fe053c6d08a6ace5af262a5a3088c8bce816e1
ko <kojavaee@aliyun.com>
2022-01-23 06:47:47 +0800

    add test coroutine

--- coroutine/test_coroutine.go
+++ coroutine/test_coroutine.go
@@ -2,12 +2,27 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"sync"
 	"time"
 )
 
 var waitGroup = &sync.WaitGroup{}
+var mutex = &sync.Mutex{}
+var n = 0
+
+func testAddN() {
+	n++
+	fmt.Println(n)
+}
+
+func lockTestAddN() {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	n++
+	fmt.Println(n)
+	time.Sleep(time.Second * 1)
+}
 
 func say(text string) {
 	fmt.Println(text)
@@ -32,13 +47,25 @@ func main() {
 	//}
 	//waitGroup.Wait()
 
-	times := 5
-	waitGroup.Add(times)
-	for i := 0; i < times; i++ {
-		go func() {
-			fmt.Println("hello,go func " + strconv.Itoa(i))
-			waitGroup.Done()
-		}()
-	}
-	waitGroup.Wait()
+	//times := 5
+	//waitGroup.Add(times)
+	//for i := 0; i < times; i++ {
+	//	go func() {
+	//		fmt.Println("hello,go func " + strconv.Itoa(i))
+	//		waitGroup.Done()
+	//	}()
+	//}
+	//waitGroup.Wait()
+
+	//for i := 0; i < 5; i++ {
+	//	go testAddN()
+	//}
+	//time.Sleep(time.Second * 5)
+	//fmt.Println(n)
+
+	//for i := 0; i < 5; i++ {
+	//	go lockTestAddN()
+	//}
+	//time.Sleep(time.Second * 8)
+	//fmt.Println(n)
 }
