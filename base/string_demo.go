4b8e368248948753886fe0f3f7b09e978bec3fe7
ko <kojavaee@aliyun.com>
2022-02-13 21:28:36 +0800

    添加字符

---
+++ base/string_demo.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	c1 := 'a'
+	fmt.Println(c1) //97
+	c2 := '6'
+	fmt.Println(c2) //54
+	c3 := '-'
+	fmt.Println(c3) //45
+	c4 := 'c'
+	fmt.Println(c4) //99
+	c5 := '中'
+	fmt.Println(c5)         //20013
+	fmt.Printf("%c \n", c5) //中
+
+}
