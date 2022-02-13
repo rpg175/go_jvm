75a9838639420b8b7ce768e94af5ab2821e8bbfa
ko <kojavaee@aliyun.com>
2022-02-13 21:40:01 +0800

    添加golang 转义字符

--- base/string_demo.go
+++ base/string_demo.go
@@ -15,4 +15,18 @@ func main() {
 	fmt.Println(c5)         //20013
 	fmt.Printf("%c \n", c5) //中
 
+	//转义字符
+	//\n 换行
+	fmt.Println("aaa\nbbb")
+	//\b 退格
+	fmt.Println("aaa\bbbb")
+	//\r 光标回到本行开头，后续输入就会替换原有的字符
+	fmt.Println("aaaaa\rbbb")
+	//\t 制表符
+	fmt.Println("aaa\tbb")
+	//\"
+	fmt.Println("\"Golang\"")
+	//\'
+	fmt.Println("'Golang'")
+
 }
